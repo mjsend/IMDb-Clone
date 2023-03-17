24de3ff522f109bb06c6f661d66cf34dc94fb74d
Manish Jain <mjsend@gmail.com>
2023-03-17 22:24:26 +0530

    Installed Next Js

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'IMDb Clone',
+  description: 'This is the IMDb Clone website',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
